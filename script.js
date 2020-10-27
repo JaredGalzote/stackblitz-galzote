8595770e3850cb5a5624fe43824785e7883e18d3
JaredGalzote <jaredgalzote@student.laverdad.edu.ph>
2020-10-27 17:54:00 +0800

    3 files modified

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-console.log("Thankyou");
+function Submitform() {
+  var name = document.getElementById("name").value;
+  var email = document.getElementById("email").value;
+  alert("Thank you " + name + " (" + email + "), We received your message.");
+}
